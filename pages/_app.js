b62c04ab53c4bb16cf7efa444bfcce5579b53bb0
Tim Neutkens <timneutkens@me.com>
2018-11-09 17:20:16 +0100

    Disable authentication because of static deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import App, { Container } from 'next/app'
 import React from 'react'
-import authenticate from '../lib/authenticate'
+// import authenticate from '../lib/authenticate'
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {}
-    const req = ctx.req
+    // const req = ctx.req
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    const { user } = await authenticate({ req })
+    // const { user } = await authenticate({ req })
+    const { user } = {}
     return { pageProps, user }
   }
 
