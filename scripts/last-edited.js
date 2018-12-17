4b5203d894babd87f0d3c20492a879439e37bd82
Michael Novotny <manovotny@gmail.com>
2018-12-17 16:14:56 -0600

    Fixes last edited dates. (#383)
    * Fixes last edited dates.
    
    * Refactors last edited path.
    
    * Adds missing mdx files to last edited.

--- scripts/last-edited.js
+++ scripts/last-edited.js
@@ -1,38 +1,51 @@
+const execa = require('execa')
 const { resolve } = require('path')
 const fs = require('fs')
 const glob = require('glob')
 const { promisify } = require('util')
 const logger = console
 
-const getStats = promisify(fs.stat)
 const writeFile = promisify(fs.writeFile)
 const globPromise = promisify(glob)
+const getGitDate = async file =>
+  new Date(
+    await execa.stdout('git', ['log', '-1', '--format="%ad"', '--', file])
+  ).toISOString()
+const lastEditedPath = 'lib/data/last-edited.json'
 
 async function getFileStats() {
-  const files = await globPromise(resolve('./pages/docs/**/*.{js,md}'))
+  const files = await globPromise(resolve('./pages/docs/**/*.{js,md,mdx}'))
 
   const stats = await Promise.all(
     files
       .map(file => file.split('/pages/'))
       .map(file => file[1])
       .map(file => `pages/${file}`)
-      .map(file => Promise.all([getStats(file), Promise.resolve(file)]))
+      .map(file => Promise.all([getGitDate(file), Promise.resolve(file)]))
   )
 
-  return stats
-    .map(([stat, filePath]) => [stat.mtime, filePath])
-    .reduce(
-      (results, [stat, filePath]) => ({ ...results, [filePath]: stat }),
-      {}
-    )
+  return stats.reduce(
+    (results, [stat, filePath]) => ({ ...results, [filePath]: stat }),
+    {}
+  )
 }
 
 async function main(fileStats) {
-  const filePath = resolve('./lib/data/last-edited.json')
+  const filePath = resolve(`./${lastEditedPath}`)
   await writeFile(filePath, JSON.stringify(fileStats, null, 2), 'utf8')
   logger.log('Last edited file updated')
 }
 
+async function commit() {
+  const modified = await execa.stdout('git', ['ls-files', '-m'])
+
+  if (modified.includes(lastEditedPath)) {
+    await execa('git', ['add', lastEditedPath])
+    await execa('git', ['commit', '--amend', '--no-edit'])
+  }
+}
+
 getFileStats()
   .then(main)
+  .then(commit)
   .catch(error => logger.error(error))
