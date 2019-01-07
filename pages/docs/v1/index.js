ede36355582abdf70ae1d6e4640dd5da50165afa
Michael Novotny <manovotny@gmail.com>
2019-01-06 18:27:37 -0600

    Renames doc `.md` files to `.mdx`. (#398)
    * Renames doc md files to mdx.
    
    * Fixes introduction import.
    
    * Fixes `editUrl` for updated `.md` to `.mdx` files.

--- pages/docs/v1/index.js
+++ pages/docs/v1/index.js
@@ -1,3 +1,3 @@
-import IntroductionToNow from './getting-started/introduction-to-now.md'
+import IntroductionToNow from './getting-started/introduction-to-now.mdx'
 
 export default IntroductionToNow
