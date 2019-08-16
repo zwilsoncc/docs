eea711e2dc4cfe6ac1c1ddd1b28784192f15e422
Matthew Sweeney <mail@msweeneydev.com>
2019-08-16 12:21:29 +0100

    Add Limits Page (#1136)
    * initial component and file
    
    * add map
    
    * remove render wrapper
    
    * add limits page
    
    * remove unused import
    
    * remove logs
    
    * add example
    
    * add examples
    
    * improve examples
    
    * change to seconds
    
    * add link from api rate limits
    
    * update kodiak label
    
    * update example

---
+++ components/limits/limits.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import { API_LIMITS } from '../../lib/constants'
+import { InlineCode } from '../text/code'
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableRowCell,
+  TableHeadCell
+} from '~/components/table'
+
+function Limits() {
+  const [limits, setLimits] = useState([])
+  useEffect(() => {
+    async function getLimits() {
+      const res = await fetch(API_LIMITS)
+      const data = await res.json()
+      setLimits({ ...data })
+      return null
+    }
+    getLimits()
+  }, [])
+  console.log(limits)
+  return limits && limits.rateLimits ? (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeadCell className="head-cell">Description</TableHeadCell>
+          <TableHeadCell className="head-cell">Limit</TableHeadCell>
+          <TableHeadCell className="head-cell">
+            Duration (Seconds)
+          </TableHeadCell>
+          <TableHeadCell className="head-cell">Scope</TableHeadCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {limits.rateLimits.map(l => (
+          <TableRow>
+            <TableRowCell>{l.description}</TableRowCell>
+            <TableRowCell>{l.max}</TableRowCell>
+            <TableRowCell>{l.duration / 1000}</TableRowCell>
+            <TableRowCell>
+              <InlineCode>{l.scope}</InlineCode>
+            </TableRowCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ) : null
+}
+
+export default Limits
