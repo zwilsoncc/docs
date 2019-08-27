23fa36d16ea977fce7f32a2bf2326a875e56d78d
timothyis <hello@timothy.is>
2019-08-27 13:25:33 +0100

    Update amp behaviour for the sidebar (and fix zenmode)

--- components/layout/content.js
+++ components/layout/content.js
@@ -3,25 +3,30 @@ import ZenContext from '~/lib/zen-context'
 
 const getContent = (zenModeActive, children) => {
   return (
-    <div className={cn(zenModeActive ? 'content-zen-mode' : '')}>
+    <div className={cn('content', zenModeActive ? 'content-zen-mode' : '')}>
       {children}
       <style jsx>{`
-        div {
+        .content {
           display: flex;
           flex-direction: column;
           flex: 0 0 100%;
-          max-width: calc(100% - 280px);
           padding-bottom: 64px;
-          padding-left: 24px;
+          max-width: 100%;
+        }
+
+        :global(.sidebar) + .content {
           margin-left: auto;
+          max-width: calc(100% - 280px);
+          padding-left: 24px;
         }
 
         .content-zen-mode {
           padding-left: 0;
+          margin-left: 0;
         }
 
         @media screen and (max-width: 950px) {
-          div {
+          .content {
             padding-left: 0;
             max-width: 100%;
           }
