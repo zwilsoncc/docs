d46139059d63410ebb64be1d4cc1a98960810aaa
Timothy <1695613+timothyis@users.noreply.github.com>
2019-11-11 11:19:20 +0000

    Change "Advanced" to "More" (#1456)
    * Change Advanced to More
    
    * Update meta

--- pages/docs/v2/advanced/domains/dns.js
+++
@@ -1,3 +0,0 @@
-import redirect from '~/lib/redirect'
-
-export default redirect('/docs/now-cli/#commands/dns')
--- pages/docs/v2/domains-and-aliases/transferring-a-domain-in.js
+++ pages/docs/v2/domains-and-aliases/transferring-a-domain-in.js
@@ -1,3 +1,3 @@
 import redirect from '~/lib/redirect'
 
-export default redirect('/docs/v2/advanced/domains/transferring-a-domain')
+export default redirect('/guides/transferring-domains-to-zeit-now')
