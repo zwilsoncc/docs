e2ef729969f6aa162e5861bfc5a4246b2e1824ef
JJ Kasper <jj@jjsweb.site>
2020-01-16 11:51:08 -0600

    Migrate redirects/rewrites from now.json to next.config.js (#1541)
    * Migrate redirects from now.json to next.config.js
    
    * Migrate rewrites from now.json to next.config.js
    
    * Update some routes in next.config.js
    
    * Update some more routes
    
    * Un-migrate route for now
    
    * Update test
    
    * Remove script from testing
    
    * Update routes from suggestions
    
    * Update trailing slash redirect
    
    * Migrate changed routes
    
    * Update to use new permanent option
    
    * Update next.config.js
    
    Co-Authored-By: Connor Davis <mail@connordav.is>
    
    * Make trailing-redirect permanent
    
    * Update api redirect
    
    Co-authored-by: Connor Davis <mail@connordav.is>

--- next.config.js
+++ next.config.js
@@ -13,10 +13,231 @@ module.exports = withMDX({
   target: 'serverless',
 
   experimental: {
-    css: true
+    css: true,
+
+    rewrites() {
+      return [
+        // TODO: test out basePath to replace below rewrites
+        {
+          source: '/docs/sitemap.xml',
+          destination: '/sitemap.xml'
+        },
+        {
+          source: '/docs/_next/:path*',
+          destination: '/_next/:path*'
+        },
+        {
+          source: '/docs/static/:path*',
+          destination: '/static/:path*'
+        }
+      ]
+    },
+
+    redirects() {
+      return [
+        {
+          source: '/:path+/',
+          permanent: true,
+          destination: '/:path+'
+        },
+        {
+          source: '/api',
+          permanent: true,
+          destination: '/docs/api'
+        },
+        {
+          source: '/api/(.*)',
+          permanent: true,
+          destination: '/docs/api'
+        },
+        {
+          source: '/docs/builders',
+          permanent: true,
+          destination: '/docs/runtimes'
+        },
+        {
+          source: '/docs/github',
+          permanent: true,
+          destination: '/docs/v2/advanced/now-for-github'
+        },
+        {
+          source: '/docs/aliasing',
+          permanent: true,
+          destination: '/docs/v2/aliasing-a-deployment'
+        },
+        {
+          source: '/docs/v2/routing/:path*',
+          permanent: true,
+          destination: '/docs/v2/network/:path*'
+        },
+        {
+          source: '/docs/features/:path*',
+          permanent: true,
+          destination: '/docs/v1/features/:path*'
+        },
+        {
+          source: '/docs/static-deployments/:path*',
+          permanent: true,
+          destination: '/docs/v1/static-deployments/:path*'
+        },
+        {
+          source: '/docs/deployment-types/:path*',
+          permanent: true,
+          destination: '/docs/v1/deployment-types/:path*'
+        },
+        {
+          source: '/docs/(|v1/)guides/updating-now-cli',
+          permanent: true,
+          destination: '/guides/updating-now-cli'
+        },
+        {
+          source: '/docs/guides/(.*)',
+          permanent: true,
+          destination: '/guides'
+        },
+        {
+          source: '/docs/getting-started/:path*',
+          permanent: true,
+          destination: '/docs/v1/getting-started/:path*'
+        },
+        {
+          source: '/docs/v2/getting-started/(.*)',
+          permanent: true,
+          destination: '/docs/v2/introduction'
+        },
+        {
+          source:
+            '/docs/v2/((?:deployments|advanced))/builders/developer-guide(.*)',
+          permanent: true,
+          destination:
+            'https://github.com/zeit/now/blob/master/DEVELOPING_A_RUNTIME.md'
+        },
+        {
+          source: '/docs/v2/deployments/((?:builders|official-builders))/(.*)',
+          permanent: true,
+          destination: '/docs/runtimes'
+        },
+        {
+          source: '/docs/v2/advanced/builders(.*)',
+          permanent: true,
+          destination: '/docs/runtimes'
+        },
+        {
+          source: '/docs/v2/deployments/environment-variables-and-secrets',
+          permanent: true,
+          destination: '/docs/v2/build-step/#using-environment-variables-and-secrets'
+        },
+        {
+          source: '/docs/v2/advanced/concepts(.*)',
+          permanent: true,
+          destination: '/docs/v2/platform/deployments'
+        },
+        {
+          source: '/docs/v2/deployments/routes(.*)',
+          permanent: true,
+          destination: '/docs/configuration#routes'
+        },
+        {
+          source: '/docs/v2/deployments/builds',
+          permanent: true,
+          destination: '/docs/runtimes'
+        },
+        {
+          source: '/docs/v2/deployments/(.*)',
+          permanent: true,
+          destination: '/docs/v2/introduction'
+        },
+        {
+          source: '/docs/v2/domains-and-aliases/aliasing-a-deployment',
+          permanent: true,
+          destination: '/docs/v2/custom-domains#deploying-with-your-domain'
+        },
+        {
+          source: '/docs/v2/domains-and-aliases/aliasing-a-deployment',
+          permanent: true,
+          destination: '/docs/v2/custom-domains#deploying-with-your-domain'
+        },
+        {
+          source: '/docs/v2/domains-and-aliases/cdn',
+          permanent: true,
+          destination: '/docs/v2/serverless-functions/edge-caching'
+        },
+        {
+          source: '/docs/other/:path*',
+          permanent: true,
+          destination: '/docs/v1/other/:path*'
+        },
+        {
+          source: '/docs/clients/:path*',
+          permanent: true,
+          destination: '/docs/v1/clients/:path*'
+        },
+        {
+          source: '/examples/nodejs-express',
+          permanent: true,
+          destination: '/examples/express'
+        },
+        {
+          source: '/examples(.*)',
+          permanent: true,
+          destination: 'https://github.com/zeit/now/tree/master/examples'
+        },
+        {
+          source: '/docs/v1/examples/(.*)',
+          permanent: true,
+          destination: 'https://github.com/zeit/now/tree/master/examples'
+        },
+        {
+          source: '/docs/v2/platform/global-configuration(.*)',
+          permanent: true,
+          destination: '/docs/configuration#global'
+        },
+        {
+          source: '/docs/v2/advanced/platform/:path*',
+          permanent: true,
+          destination: '/docs/v2/platform/:path*'
+        },
+        {
+          source: '/docs/v2/development/basic/(.*)',
+          permanent: true,
+          destination:
+            '/docs/v2/deployment/serverless-functions#local-development'
+        },
+        {
+          source: '/docs/v2/development/environment-variables(.*)',
+          permanent: true,
+          destination: '/docs/v2/serverless-functions/env-and-secrets'
+        },
+        {
+          source: '/docs/v2/development/(.*)',
+          permanent: true,
+          destination: '/docs/v2/serverless-functions/introduction'
+        },
+        {
+          source: '/docs/v2/integrations/:path*',
+          permanent: true,
+          destination: '/docs/v2/more/:path*'
+        },
+        {
+          source: '/docs/v2/domains-and-aliases/(.*)',
+          permanent: true,
+          destination: '/docs/v2/custom-domains'
+        },
+        {
+          source: '/docs/v2/advanced/:path*',
+          permanent: true,
+          destination: '/docs/v2/more/:path*'
+        },
+        {
+          source: '/docs/version-detection',
+          permanent: true,
+          destination:
+            '/docs/v2/platform/frequently-asked-questions#platform-version-detection'
+        }
+      ]
+    }
   },
 
-  exportTrailingSlash: true,
 
   // Allow mdx and md files to be pages
   pageExtensions: ['jsx', 'js', 'mdx', 'md'],
