80230827aa81db22139f9c2177b5b0eb3c4206f8
Shu Uesugi <shu@chibicode.com>
2020-03-03 11:20:26 -0800

    Fix Algolia indexing for references/API (#1678)
    * Fix Algolia indexing
    
    * Use api.html instead of v2
    
    * Index Algolia temporarily
    
    * Revert "Index Algolia temporarily"
    
    This reverts commit 47d63fd351bb2274e972df856d21db87fab991c4.

--- scripts/index-docs.js
+++ scripts/index-docs.js
@@ -41,11 +41,11 @@ async function main() {
     files = await globby([
       'dist/guides/**/*.html',
       'dist/docs/v2/**/*.html',
-      'dist/docs/api/v2/**/*.html',
-      'dist/docs/integrations/v2/**/*.html',
-      'dist/docs/now-cli/**/*.html',
-      'dist/docs/configuration/**/*.html',
-      'dist/docs/runtimes/**/*.html'
+      'dist/docs/api.html',
+      'dist/docs/integrations.html',
+      'dist/docs/now-cli.html',
+      'dist/docs/configuration.html',
+      'dist/docs/runtimes.html'
     ])
     // filter out AMP pages
     files = files.filter(f => f.indexOf('.amp/index.html') < 0)
@@ -55,12 +55,12 @@ async function main() {
 
   // Loop through files
   files.forEach(file => {
-    const isAPISection = !!file.startsWith('dist/docs/api/v2')
+    const isAPISection = !!file.startsWith('dist/docs/api')
     const isRefSection =
       !!file.startsWith('dist/docs/now-cli') ||
       !!file.startsWith('dist/docs/runtimes') ||
       !!file.startsWith('dist/docs/configuration') ||
-      !!file.startsWith('dist/docs/integrations/v2')
+      !!file.startsWith('dist/docs/integrations')
     const isDocs = !!file.startsWith('dist/docs/v2')
     const isGuides = !!file.startsWith('dist/guides')
 
