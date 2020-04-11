1b45a0b7b68e4b8f405334a2f7ab5b4f907cf285
Shu Uesugi <shu@chibicode.com>
2020-04-11 15:12:24 -0700

    Add ProductNameShort

---
+++ components/product-name/short.js
@@ -0,0 +1,5 @@
+import { PRODUCT_SHORT } from '~/lib/constants'
+
+const ProductNameShort = () => PRODUCT_SHORT
+
+export default ProductNameShort
--- scripts/generate-site-map.js
+++ scripts/generate-site-map.js
@@ -74,7 +74,8 @@ function xmlUrlNode(pagePath) {
         match[1]
           .replace(/\${PRODUCT}/g, productNames.product)
           .replace(/\${PRODUCT_SIMPLE}/g, productNames.productSimple)
-          .replace(/\${CDN_NAME}/g, productNames.cdnName) +
+          .replace(/\${CDN_NAME}/g, productNames.cdnName)
+          .replace(/\${PRODUCT_SHORT}/g, productNames.productShort) +
         ')'
     )
 
