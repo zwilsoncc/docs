013c2158e1fdd1c4a4aa24b527fb9f10192fdf81
Shu Uesugi <shu@chibicode.com>
2020-04-12 20:56:20 -0700

    Use 1.0

--- pages/docs/api/v1/index.js
+++ pages/docs/api/v1/index.js
@@ -1,12 +1,12 @@
 import ApiDocs from '~/components/references-mdx/api/v1/index.mdx'
 import ReferenceLayout from '~/components/layout/reference'
-import { PRODUCT } from '~/lib/constants'
+import { PRODUCT_V1 } from '~/lib/constants'
 
 export default () => (
   <ReferenceLayout
     Data={<ApiDocs />}
     versioned
-    description={`A guide to the legacy ${PRODUCT} API.`}
-    title={`${PRODUCT} 1.0 API Documentation`}
+    description={`A guide to the legacy ${PRODUCT_V1} API.`}
+    title={`${PRODUCT_V1} API Documentation`}
   />
 )
