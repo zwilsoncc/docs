c85ea2082d8a0564c64871d51a97e30adfc9e927
Shu Uesugi <shu@chibicode.com>
2020-04-13 00:39:35 -0700

    Continue updating ProductNameShort

--- pages/docs/now-cli/index.js
+++ pages/docs/now-cli/index.js
@@ -1,10 +1,11 @@
 import NowCLIDocs from '~/components/references-mdx/now-cli/index.mdx'
 import ReferenceLayout from '~/components/layout/reference'
+import { PRODUCT_SHORT } from '~/lib/constants'
 
 export default () => (
   <ReferenceLayout
     Data={<NowCLIDocs />}
-    title="Now CLI Reference"
-    description="A complete reference of the Now CLI detailing the available commands, their usage and optional flags"
+    title={`${PRODUCT_SHORT} CLI Reference`}
+    description={`A complete reference of the ${PRODUCT_SHORT} CLI detailing the available commands, their usage and optional flags`}
   />
 )
