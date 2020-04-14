7afe5618957d635e64fff5ec9b5dd72d90d38baa
Shu Uesugi <shu@chibicode.com>
2020-04-13 17:02:08 -0700

    Update file names

--- components/name/name-wrapper.js
+++ components/name/name-wrapper.js
@@ -1,5 +1,5 @@
 // Use this component for headings so that
 // lib/api/runtime/slugify-title.js works as intended
-const ProductNameWrapper = ({ name }) => name
+const NameWrapper = ({ name }) => name
 
-export default ProductNameWrapper
+export default NameWrapper
