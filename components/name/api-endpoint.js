59b0c55ff30c09fcfcef1889b5f9bb57f823e37f
Shu Uesugi <shu@chibicode.com>
2020-04-20 16:14:43 -0700

    Variablize API Endpoint (#1768)
    * Add ApiEndpoint component
    
    * Update authentication.mdx
    
    * Update aliases.mdx
    
    * Update authentication.mdx
    
    * Search/replace url="...."
    
    * Update remaining strings

---
+++ components/name/api-endpoint.js
@@ -0,0 +1,6 @@
+import { API_ENDPOINT } from '~/lib/constants'
+import { InlineCode } from '~/components/code'
+
+const ApiEndpoint = () => <InlineCode>{API_ENDPOINT}</InlineCode>
+
+export default ApiEndpoint
